9ebe45df62f2088d63219108f79a53ae98462328
Yarynka Shyika <pinkpoppy.art@gmail.com>
2018-04-03 23:25:34 +0300

    Created endpoint for tweets timeline

---
+++ server/twitterconfig.js
@@ -0,0 +1,6 @@
+﻿module.exports = {
+    consumer_key: 'iLM7jiWk56R5C1Q438Oi7amfi',
+    consumer_secret: 'yYrBa47eVeAKPllm1INYRI9DsJjQk8DzbvkSVp2rvAuIiZzfdl',
+    access_token: '997571090-Rn68N42cf57zFRxiHAKNPoZquYehDB6suhpm8eIA',
+    access_token_secret: 'GBPo2O5c3xroQTaCWFEoUo9EeQMXosd6J0gramjIjyU4m'
+};
