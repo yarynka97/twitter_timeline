99cc007c4a17836d3caff7ba36a0064ee616afd4
Yarynka Shyika <pinkpoppy.art@gmail.com>
2018-04-09 17:48:56 +0300

    Fixed config file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 ﻿module.exports={
   "mistakeImg": "http://i2.cdn.turner.com/money/dam/assets/141224103109-investor-mistake-1024x576.jpg",
   "logo": "http://logos-download.com/wp-content/uploads/2016/02/Twitter_logo_bird_transparent_png-700x568.png",
-  "serverUrl": "http://localhost:8080/api/tweets",
-  "extra": "https://timeline-for-tweets.herokuapp.com/api/tweets",
+  "serverUrl": "https://timeline-for-tweets.herokuapp.com/api/tweets",
   "twitterKeys": {
       consumer_key: process.env.CONSUMER_KEY,
       consumer_secret: process.env.CONSUMER_SECRET,
